f4aef60d40f7a24d6e0cfb990c38a4edfb4d37ee
Tarek <tarekipad@gmail.com>
2020-12-11 13:05:05 +0100

    cleaning up comments. Will add to readme.md.

--- index.js
+++ index.js
@@ -1,150 +1,9 @@
 "use strict";
-//======================================================
-//======================================================
-//======================================================
-// FILE MANAGEMENT
-//
-// we store it in the fs variable (can be any variable) which we can use later
-// this is how we use a module, we == require it
-// once we reqire it we can now can get access to functoins of it to read and write data to the file system.
+
 const fs = require("fs");
-
-// const hello = "Hello world";
-// console.log(hello);
-// to run the code in the terminal we run node
-
-// we required a new module (fs module)
-
-//========= how to read data from files =================
-// readfile sync takes 2 arguments, first the path to the file we are reading and second the character
-// const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
-
-// // we are storing the data from the text file in this variable textIn.
-// console.log(textIn);
-
-//======================================================
-// how to write data to files
-// const textOut = `Hello there, this is a string i put into the data. This is what we know about avocados: ${textIn}.\n Created on ${Date.now()}`;
-
-// // we have now created a new file called output with the text inside it and the date
-// fs.writeFileSync("./txt/output.txt", textOut);
-// console.log("File Written");
-//======================================================
-//This code we wrote to read and save text into a variable - was a syncronous wasy. Each statemetn process line by line. Each line blocks the execution of the next.
-// Blocking code
-// step 1
-// const fs = require("fs");
-// step 2
-// const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
-// step 3
-// console.log(textIn);
-//
-//
-//
-//Solution is to use Asynchronous non-blocking code
-// offload heavy work to be worked on in the background and call back function is then called to handle the result and the rest of the code can execute.
-
-// we can defer reaction in the future to make the code non blocking
-
-// readFile function is asynchronous which accepts callback functions. It will start reading the file in the background and then move on to the nxt statement printing to the console.
-// fs.readFile("input.txt", "utf-8", (err, data) => {
-//   console.log(data);
-// });
-// console.log("Reading File...");
-
-//======================================================
-// Node js needs to operate around non-blocking I/o model (input/output model)
-// we will use many callback functions
-
-// using callbacks doesn always make it asynchronous
-// promises and async/await can help with callback hell
-//======================================================
-// reading and wirting files in asynchronous way
-// non blocking
-
-// fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
-//   fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
-//     console.log(data2);
-//     fs.readFile(`./txt/append.txt`, "utf-8", (err, data3) => {
-//       console.log(data3);
-
-//       fs.writeFile("/txt/final.txt", `${data2}\n${data3}`, "utf-8", (err) => {
-//         console.log("File has been Written");
-//       });
-//     });
-//   });
-// });
-// console.log("Will read file");
-// =================================================================
-// =================================================================
-// =================================================================
-// SERVER MANAGEMENT
-
 // Creating a web server - include a module HTTP - gives us networking capabilites such as building a http server
 const http = require("http");
-
-// STEP 1 CREATE SERVER
-//each time a new request comes to the server this call back function below will be called - this is how to send back a plain text response when a request comes to the server
-// const server = http.createServer((req, res) => {
-//   res.end("hello from the server");
-// });
-
-// // STEP 2 START SERVER
-// // need to assign the server to a variable (server)
-
-// // this will start listening for incoming requests from the client
-// server.listen(8000, "127.0.0.1", () => {
-//   console.log("Listening to requests on port 8000");
-// });
-// =================================================================
-// =================================================================
-// =================================================================
-// ROUTING
-// //IMPLEMENTING DIFFERENT ACTIONS FOR DIFFERENT URLS
-
-// // STEP 1 - ANALYSE THE URL - WE NEED TO ADD ANOTHER MODULE - URL
-// const url = require("url");
-
-// const server = http.createServer((req, res) => {
-//   const pathName = req.url;
-
-//   if (pathName === "/" || pathName === "/overview") {
-//     res.end("this is the overview"); //IF OVERVIEW URL ENTERED
-//   } else if (pathName === "/product") {
-//     res.end("this is the product"); //IF PRODUCT URL ENTERED
-//   } else {
-//     // headers needs to be set before the response is sent
-//     res.writeHead(404, {
-//       // THESE ARE WHERE YOU SPECIFY HEADERS
-//       "Content-type": "text/html", //INFORMS BROWSER TO EXPECT A HEADER IN HTML
-//       "MY-own-header": "hello-world", //WE CAN MAKE OUR OWN HEADERS
-//     });
-//     res.end("<h1>PAGE NOT FOUND</h1>"); //THE RESPONSE IF NO PAGE FOUND
-//   }
-// });
-
-// // LISTENING FOR REQUESTS ON THE SERVER
-// server.listen(8000, "127.0.0.1", () => {
-//   console.log("Listening to requests on port 8000");
-// });
-
-// the url module is used for more complicated applications. It can help pars the specific urls and their codes etc into easy to read objects
-//
-//
-//
-//
-// =================================================================
-// =================================================================
-// =================================================================
-// APIS
-// APIS WILL SEND JSON DATA TO THE CLIENT. SO WE ADD AN ELSE IF TO SHOW A RESPONSE WHEN THE URL CONTAINES /API
-
-// WE READ DATA FROM JSON FILE, PARSE THE JSON DATA USING JS, PASS THE DATA BACK TO TO THE CLIENT/USER
-
 const url = require("url");
-
-// even though this is sychronous it will not be bloccking anything as it only has to be run once so the data is parsed once and does not ahve to be parsed each time a user clicks on going to the API page.
-// before anything happens it will read the json file and put the data into variable data
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 // it will parse the data into an array - productData is an array of the data in the json file
 const productData = JSON.parse(data);
@@ -209,14 +68,6 @@ const server = http.createServer((req, res) => {
 
     // THIS IS THE API
   } else if (pathname === "/api") {
-    // fs.readFile(`${__dirname}/dev-data/data.json`, "utf-8", (err, data) => {
-    //   // data is in json. need to parse is
-    //   const productData = JSON.parse(data);
-    //   //need to tell the browser we are sending back json data
-    //   res.writeHead(200, { "Content-type": "application/json" });
-    //   // send back the data as a response
-    //   res.end(data);
-    // });
     //need to tell the browser we are sending back json data
     res.writeHead(200, { "Content-type": "application/json" });
     // send back the data as a response
